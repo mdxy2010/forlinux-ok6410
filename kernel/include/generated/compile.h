f511839529869bf2be4431096ece2ff698ba6876
mdxy2014 <mdxy2014@gmail.com>
2016-01-15 20:37:01 +0800

    sync ok6410 kernel and u-boot source code to remote

---
+++ kernel/include/generated/compile.h
@@ -0,0 +1,6 @@
+/* This file is auto generated, version 6 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#6 Thu Jan 14 01:00:32 CST 2016"
+#define LINUX_COMPILE_BY "linux"
+#define LINUX_COMPILE_HOST "ThinkPad-E550"
+#define LINUX_COMPILER "gcc version 4.3.2 (Sourcery G++ Lite 2008q3-72) "
---
+++ u-boot/linuxy_build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+make forlinx_nand_ram256_config 
+
+make ARCH=arm CROSS_COMPILE=arm-linux-  
+
